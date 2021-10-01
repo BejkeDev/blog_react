f50a06da5f5482b0bb0ed6a51437352c7682c79c
BejkeDev <bejkezaganjori@gmail.com>
2021-10-01 23:38:49 +0200

    first commit blog react

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts=()=>async despatch=>{
+  const responseposts= await jsonPlaceholder.get('/posts');
+
+    despatch({type:'FETCH_POSTS',payload:responseposts.data})
+  }
