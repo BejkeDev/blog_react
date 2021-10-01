d1efb20a83cfdbbca330f203bd8721257dd54ed0
BejkeDev <bejkezaganjori@gmail.com>
2021-10-02 01:11:42 +0200

    edit index.js actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,5 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts=()=>async despatch=>{
   const responseposts= await jsonPlaceholder.get('/posts');
-
     despatch({type:'FETCH_POSTS',payload:responseposts.data})
   }
